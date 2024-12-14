9cea992b88e5dda8726eab06fd8227b03721a75c
Your Name <you@example.com>
2024-12-14 18:45:43 +0300

    record

--- src/app/mplayer/mplayer.component.ts
+++ src/app/mplayer/mplayer.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemReorderEventDetail } from '@ionic/angular';
+
+interface RecordEl{
+  name: string
+  url: string
+}
 
 @Component({
   selector: 'app-mplayer',
@@ -6,6 +12,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mplayer.component.scss'],
 })
 export class MplayerComponent  implements OnInit {
+  
+  records: RecordEl[] = [
+    {name: "uytuyt", url: "hhkijkjh"},
+    {name: "uytuyt", url: "hhkijkjh"},
+  ]
   public progress = 0;
 
   constructor() {
@@ -16,7 +27,18 @@ export class MplayerComponent  implements OnInit {
       }
     }, 100);
   }
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
 
   ngOnInit() {}
+
 
 }
